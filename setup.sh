6a2991267ec0d0611e38a2399cdd541ca821347f
Flobeey <86659758+Flobeey@users.noreply.github.com>
2024-02-02 09:50:25 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,27 +6,39 @@ sudo apt upgrade -y
 sudo apt dist-upgrade
 
 # Install initial tools
-sudo apt install git apt-transport-https btop pip vim -y
+sudo apt install git apt-transport-https btop pip vim gem -y
 
 # Install python and remove environment requirement
 sudo apt install python3-full -y
 sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
 sudo rm /usr/lib/python3.12/EXTERNALLY-MANAGED
 
-# Add the kali packages
-sudo sh -c "echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list"
+# Ask the user if this is Kali Linux
+read -p "Is this Kali Linux? (y/n): " is_kali
 
-# Add the public key for kali
-wget -qO - 'https://archive.kali.org/archive-key.asc' | sudo apt-key add -
+# Check user input
+if [ "$is_kali" = "y" ]; then
+    # Add the Kali packages
+    sudo sh -c "echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list"
 
-# Add the low priority to the kali packages
-sudo touch /etc/apt/preferences.d/kali.pref
-sudo sh -c "echo 'Package: *'>/etc/apt/preferences.d/kali.pref; echo 'Pin: release a=kali-rolling'>>/etc/apt/preferences.d/kali.pref; echo 'Pin-Priority: 50'>>/etc/apt/preferences.d/kali.pref"
+    # Add the public key for Kali
+    wget -qO - 'https://archive.kali.org/archive-key.asc' | sudo apt-key add -
 
-# Install aptitude
-sudo apt install aptitude -y
-# This now means you can use the following command to install tools from kali 
-# sudo aptitude install -t kali-rolling PACKAGE-NAME 
+    # Add the low priority to the Kali packages
+    sudo touch /etc/apt/preferences.d/kali.pref
+    sudo sh -c "echo 'Package: *'>/etc/apt/preferences.d/kali.pref; echo 'Pin: release a=kali-rolling'>>/etc/apt/preferences.d/kali.pref; echo 'Pin-Priority: 50'>>/etc/apt/preferences.d/kali.pref"
+
+    # Update the package list
+    sudo apt update
+
+    # Install aptitude
+    sudo apt install aptitude -y
+    # This now means you can use the following command to install tools from kali 
+    # sudo aptitude install -t kali-rolling PACKAGE-NAME 
+
+else
+    echo "Skipping Kali Linux setup."
+fi
 
 # Install ansible
 python3 -m pip install ansible
@@ -54,7 +66,7 @@ export GOMODCACHE=/home/$USER/software/go/pkg/mod
 export GOPATH=/home/$USER/software/go
 export LC_ALL=en_NZ.UTF-8
 export LANG=en_NZ.UTF-8' >> .bashrc
-source /home/$USER/.bashrc
+export PATH=$PATH:/usr/local/go/bin
 
 # Download the ansible playbook
 cd $HOME/software && git clone https://github.com/Flobeey/debian-ansible-build.git
